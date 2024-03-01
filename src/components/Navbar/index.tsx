1c4419a9ce451e9b310535117812e89c47b1a0de
Natan200-2 <natanboas21@outlook.com>
2024-03-01 15:53:57 -0300

    🎉 configurações iniciais

---
+++ src/components/Navbar/index.tsx
@@ -0,0 +1,11 @@
+import styles from './Navbar.module.scss';
+
+const Navbar = () => {
+    return(
+        <nav className={styles.nav}>
+            navbar
+        </nav>
+    )
+}
+
+export default Navbar;
