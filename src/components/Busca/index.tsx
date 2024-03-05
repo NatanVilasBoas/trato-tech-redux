b1c38f95930db90638dfa54124f7604a354665a8
Natan200-2 <natanboas21@outlook.com>
2024-03-05 13:46:33 -0300

    📦 💄 ✨ navbar criada e estilizada, busca criada e estilizada, pacote de icones instalado

---
+++ src/components/Busca/index.tsx
@@ -0,0 +1,11 @@
+import styles from './Busca.module.scss';
+
+const Busca = () => {
+    return(
+        <div className={styles.busca}>
+            <input className={styles.input} />
+        </div>
+    )
+}
+
+export default Busca;
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,9 +1,37 @@
 import styles from './Navbar.module.scss';
+import logo from '../../assets/logo.svg';
+import classNames from 'classnames';
+import { RiShoppingCart2Line, RiShoppingCartFill } from 'react-icons/ri';
+
+const iconProps = {
+    color: 'white',
+    size: 24
+}
 
 const Navbar = () => {
-    return(
+    return (
         <nav className={styles.nav}>
-            navbar
+            <img className={styles.logo} src={logo} alt='Log' />
+            <div className={styles.links}>
+                <div>
+                    <a href='/' className={classNames(styles.link, {
+                        [styles.selected]: window.location.pathname === '/'
+                    })}>
+                        Página Inicial
+                    </a>
+                </div>
+            </div>
+            <div className={styles.busca}>
+
+            </div>
+            <div className={styles.icones}>
+                <a href='/carrinho'>
+                    {window.location.pathname === '/carrinho'
+                        ? <RiShoppingCartFill {...iconProps} />
+                        : < RiShoppingCart2Line {...iconProps} />
+                    }
+                </a>
+            </div>
         </nav>
     )
 }
