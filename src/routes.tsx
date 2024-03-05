659fb7c01ddcd25f99a52b5e0ad9afcf256e357e
Natan200-2 <natanboas21@outlook.com>
2024-03-05 14:38:59 -0300

    💄 ✨ Home page e Header criados e estilizados

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PaginaPadrao from './components/PaginaPadrao';
+import Home from "./pages/Home";
 
 export default function AppRouter(){
     return(
         <BrowserRouter>
         <Routes>
             <Route path="/" element={<PaginaPadrao/>}>
-                <Route index element={<div>Home</div>}/>
+                <Route index element={<Home/>}/>
             </Route>
         </Routes>
         </BrowserRouter>
