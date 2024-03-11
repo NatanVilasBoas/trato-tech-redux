8d5dca8f898f5acd58dc672ace44b8ef921085d4
Natan200-2 <natanboas21@outlook.com>
2024-03-11 15:11:21 -0300

    🚑 corigido rerenderizações desnecessárias usando createSelector.

--- src/pages/Categoria/index.tsx
+++ src/pages/Categoria/index.tsx
@@ -1,18 +1,41 @@
 import { useParams } from "react-router-dom";
-import { useAppSelector } from "../../app/hooks";
 import Header from "../../components/Header";
 import styles from './Categoria.module.scss';
 import Item from "../../components/Item";
+import { createSelector } from "@reduxjs/toolkit";
+import { RootState } from "../../app/store";
+import { useSelector } from "react-redux";
+import { Categoria as CategoriaType } from "../../app/store/reducers/categorias";
+import { Itens } from "../../app/store/reducers/itens";
+
+// Define um seletor separado para a string de busca
+const selectBusca = (state: RootState) => state.busca;
+
+// Defina makeSelectCategoriaItens para aceitar a string de busca como argumento
+const makeSelectCategoriaItens = () =>
+    createSelector(
+        // Use useSelector para acessar a string de busca do store
+        (state: RootState) => selectBusca(state),
+        // Use seletores individuais para extrair partes relevantes do estado
+        (state: RootState) => state.categorias,
+        (state: RootState) => state.itens,
+        // Passe a string de busca como um argumento para o seletor criado por createSelector
+        (busca: string, categorias: CategoriaType[], itens: Itens[]) => {
+            const nomeCategoria = useParams().nomeCategoria;
+            const categoria = categorias.find(categoria => categoria.id === nomeCategoria);
+            if (!categoria) return { categoria: undefined, itens: [] };
+
+            const regexp = new RegExp(busca, 'i');
+            const itensFiltrados = itens.filter(item => item.categoria === nomeCategoria && item.titulo.match(regexp));
+
+            return { categoria, itens: itensFiltrados };
+        }
+    );
 
 const Categoria = () => {
-    const { nomeCategoria } = useParams();
-    const { categoria, itens } = useAppSelector(state => {
-        const regexp = new RegExp(state.busca, 'i');
-        return {
-            categoria: state.categorias.find(categoria => categoria.id === nomeCategoria),
-            itens: state.itens.filter(item => item.categoria === nomeCategoria && item.titulo.match(regexp))
-        }
-    });
+    const selectCategoriaItens = makeSelectCategoriaItens();
+    const { categoria, itens } = useSelector((state: RootState) => selectCategoriaItens(state));
+
     return (
         <div>
             {categoria ?
